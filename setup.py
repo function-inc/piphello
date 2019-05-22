b1822de872125f2ae0d35a472a91a4fff5fae8d5
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2019-05-23 03:00:49 +0900

    Create the simple pip.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='piphello',
+      version='0.1',
+      description='A greeting in the world',
+      url='https://github.com/mazgi-sandbox/piphello',
+      author='MATSUKI Hidenori',
+      author_email='matsuki.hidenori+github@gmail.com',
+      license='MIT',
+      package=['piphello'],
+      zip_safe=False)
